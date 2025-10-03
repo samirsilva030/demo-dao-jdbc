475902fa77f5a45ce6f1e5f8a485d27918436c63
Samir Silva <samirsilva030@gmail.com>
2025-10-02 21:53:38 -0300

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -37,6 +37,14 @@ public class Program {
 		Seller newSeller = new Seller(null, "Greg", "Greg@gmail.com", new Date(), 4000.0, department);
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted! new id = " + newSeller.getId());
-	}
 
+		
+		System.out.println("\n=== TEST5: seller update ====");
+		seller = sellerDao.findById(1);
+		seller.setName("Martha waine");
+		sellerDao.updade(seller);
+		System.out.println("Update completed");
+	}
+	
+		
 }
