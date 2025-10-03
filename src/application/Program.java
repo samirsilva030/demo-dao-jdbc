b5a273bacce586faf5392c97c583075df5dc745b
Samir Silva <samirsilva030@gmail.com>
2025-10-02 21:33:31 -0300

    insert implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -32,6 +32,11 @@ public class Program {
 		for (Seller obj : list) {
 			System.out.println(obj);
 		}
+		
+		System.out.println("\n=== TEST4: seller insert ====");
+		Seller newSeller = new Seller(null, "Greg", "Greg@gmail.com", new Date(), 4000.0, department);
+		sellerDao.insert(newSeller);
+		System.out.println("Inserted! new id = " + newSeller.getId());
 	}
 
 }
